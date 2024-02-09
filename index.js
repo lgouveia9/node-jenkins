1d72d10ada242d4c97d48c983d022a32fd0857aa
lgouveia9 <lgouveia_9@hotmail.com>
2024-02-09 16:05:46 -0300

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3300;
 
 app.get('/', (req, res) => {
-  res.send('Hello, DOCKER STACK DEPLOY IN DOCKER SWARM!');
+  res.send('Hello, DOCKER STACK DEPLOY IN DOCKER SWARM NEW!');
 });
 
 app.listen(port, () => {
